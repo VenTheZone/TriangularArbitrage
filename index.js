17cf155de56307c3650ac88dfb4cc36d417fdcf0
Zone <109752906+VenTheZone@users.noreply.github.com>
2023-10-08 16:37:58 +0800

    Update index.js
    Polygon re-write

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// load logger library
+// Load logger library
 const logger = require('./lib/LoggerCore');
+const Web3 = require('web3');
 
 var env = require('node-env-file');
 try {
@@ -9,65 +10,51 @@ try {
 }
 env(__dirname + '/conf.ini');
 
+const web3 = new Web3('https://rpc-mainnet.maticvigil.com');
+
 logger.info('\n\n\n----- Bot Starting : -----\n\n\n');
 
 var exchangeAPI = {};
 
 logger.info('--- Loading Exchange API');
 
-// make exchange module dynamic later
-if (process.env.activeExchange == 'binance'){
-  logger.info('--- \tActive Exchange:' + process.env.activeExchange);
-  // activePairs = process.env.binancePairs;
-
-  const api = require('binance');
-  const beautifyResponse = false;
-  exchangeAPI = new api.BinanceRest({
-    timeout: parseInt(process.env.restTimeout), // Optional, defaults to 15000, is the request time out in milliseconds
-    recvWindow: parseInt(process.env.restRecvWindow), // Optional, defaults to 5000, increase if you're getting timestamp errors
-    disableBeautification: beautifyResponse
-  });
-  exchangeAPI.WS = new api.BinanceWS(beautifyResponse);
-}
+// TODO: Add Uniswap/Polygon logic here
+// const uniswap = new Uniswap(web3); // Placeholder, actual Uniswap API initialization may differ
 
 var botOptions = {
-    UI: {
-      title: 'Top Potential Arbitrage Triplets, via: ' + process.env.binanceColumns
-    },
-    arbitrage: {
-      paths: process.env.binanceColumns.split(','),
-      start: process.env.binanceStartingPoint
-    },
-    storage: {
-      logHistory: false
-    },
-    trading: {
-      paperOnly: true,
-      // only candidates with over x% gain potential are queued for trading
-      minQueuePercentageThreshold: 3,
-      // how many times we need to see the same opportunity before deciding to act on it
-      minHitsThreshold: 5
-    }
+  UI: {
+    title: 'Top Potential Arbitrage Triplets, via Uniswap on Polygon'
   },
-  ctrl = {
-    options: botOptions,
-    storage: {
-      trading: {
-      // queued triplets
-        queue: [],
-        // actively trading triplets
-        active: []
-      },
-      candidates: [],
-      streams: [],
-      pairRanks: []
+  arbitrage: {
+    paths: process.env.uniswapColumns.split(','),
+    start: process.env.uniswapStartingPoint
+  },
+  storage: {
+    logHistory: false
+  },
+  trading: {
+    paperOnly: true,
+    minQueuePercentageThreshold: 3,
+    minHitsThreshold: 5
+  }
+},
+ctrl = {
+  options: botOptions,
+  storage: {
+    trading: {
+      queue: [],
+      active: []
     },
-    logger: logger,
-    exchange: exchangeAPI
-  };
+    candidates: [],
+    streams: [],
+    pairRanks: []
+  },
+  logger: logger,
+  exchange: exchangeAPI // This will hold your Uniswap/Polygon API later
+};
 
-ctrl.UI       = require('./lib/UI')(ctrl.options),
-ctrl.events   = require('./lib/EventsCore')(ctrl);
+ctrl.UI = require('./lib/UI')(ctrl.options),
+ctrl.events = require('./lib/EventsCore')(ctrl);
 
 // We're ready to start. Load up the webhook streams and start making it rain.
 require('./lib/BotCore')(ctrl);
